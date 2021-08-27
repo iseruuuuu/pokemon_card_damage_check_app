14ae66a416fb24d45a02bf46d216e1fd5ce57280
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-08-27 22:52:43 +0900

    add: onTapを追加

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,164 +1,205 @@
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
+import 'home_screen_state.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
   Widget build(BuildContext context) {
-    final mediaWidth = MediaQuery.of(context).size.width;
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Spacer(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('リセット'),
-                ),
-              ],
-            ),
-            const Spacer(),
-            Row(
-              children: [
-                const Spacer(),
-                SizedBox(
-                  height: mediaWidth / 1.5,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Container(
-                        width: mediaWidth / 8,
-                        height: mediaWidth / 8,
-                        color: Colors.orange,
-                        child: const Center(
-                          child: Text(
-                            'やけど',
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: mediaWidth / 8,
-                        height: mediaWidth / 8,
-                        color: Colors.deepPurple,
-                        child: const Center(
-                          child: Text(
-                            'どく',
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const Spacer(),
-                //TODO 大カード
-                Container(
-                  width: mediaWidth / 2,
-                  height: mediaWidth / 1.5,
-                  color: Colors.red,
-                ),
-                const Spacer(),
-                SizedBox(
-                  height: mediaWidth / 1.5,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Container(
-                        width: mediaWidth / 8,
-                        height: mediaWidth / 8,
-                        color: Colors.red,
-                        child: const Center(
-                          child: Text(
-                            '10',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: mediaWidth / 8,
-                        height: mediaWidth / 8,
-                        color: Colors.red,
-                        child: const Center(
-                          child: Text(
-                            '50',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: mediaWidth / 8,
-                        height: mediaWidth / 8,
-                        color: Colors.red,
-                        child: const Center(
-                          child: Text(
-                            '100',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const Spacer(),
-              ],
-            ),
-            const Spacer(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  width: mediaWidth / 5.5,
-                  height: mediaWidth / 2.5,
-                  color: Colors.red,
-                ),
-                Container(
-                  width: mediaWidth / 5.5,
-                  height: mediaWidth / 2.5,
-                  color: Colors.blue,
-                ),
-                Container(
-                  width: mediaWidth / 5.5,
-                  height: mediaWidth / 2.5,
-                  color: Colors.yellow,
-                ),
-                Container(
-                  width: mediaWidth / 5.5,
-                  height: mediaWidth / 2.5,
-                  color: Colors.green,
-                ),
-                Container(
-                  width: mediaWidth / 5.5,
-                  height: mediaWidth / 2.5,
-                  color: Colors.pink,
-                ),
-              ],
-            ),
-          ],
-        ),
+    return StateNotifierProvider<HomeScreenController, HomeScreenState>(
+      create: (context) => HomeScreenController(
+        context: context,
       ),
+      builder: (context, _) {
+        final mediaWidth = MediaQuery.of(context).size.width;
+        return Scaffold(
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('リセット'),
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                Row(
+                  children: [
+                    const Spacer(),
+                    SizedBox(
+                      height: mediaWidth / 1.5,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Container(
+                            width: mediaWidth / 8,
+                            height: mediaWidth / 8,
+                            color: Colors.orange,
+                            child: const Center(
+                              child: Text(
+                                'やけど',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: mediaWidth / 8,
+                            height: mediaWidth / 8,
+                            color: Colors.deepPurple,
+                            child: const Center(
+                              child: Text(
+                                'どく',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Spacer(),
+                    //TODO 大カード
+                    GestureDetector(
+                      onTap: () =>
+                          context.read<HomeScreenController>().pokemon(),
+                      child: Container(
+                        width: mediaWidth / 2,
+                        height: mediaWidth / 1.5,
+                        color: Colors.red,
+                      ),
+                    ),
+                    const Spacer(),
+                    SizedBox(
+                      height: mediaWidth / 1.5,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          GestureDetector(
+                            onTap: () =>
+                                context.read<HomeScreenController>().damage10(),
+                            child: Container(
+                              width: mediaWidth / 8,
+                              height: mediaWidth / 8,
+                              color: Colors.red,
+                              child: const Center(
+                                child: Text(
+                                  '10',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () =>
+                                context.read<HomeScreenController>().damage50(),
+                            child: Container(
+                              width: mediaWidth / 8,
+                              height: mediaWidth / 8,
+                              color: Colors.red,
+                              child: const Center(
+                                child: Text(
+                                  '50',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () =>
+                                context.read<HomeScreenController>().damage10(),
+                            child: Container(
+                              width: mediaWidth / 8,
+                              height: mediaWidth / 8,
+                              color: Colors.red,
+                              child: const Center(
+                                child: Text(
+                                  '100',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Spacer(),
+                  ],
+                ),
+                const Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    GestureDetector(
+                      onTap: () =>
+                          context.read<HomeScreenController>().pokemon1(),
+                      child: Container(
+                        width: mediaWidth / 5.5,
+                        height: mediaWidth / 2.5,
+                        color: Colors.red,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () =>
+                          context.read<HomeScreenController>().pokemon2(),
+                      child: Container(
+                        width: mediaWidth / 5.5,
+                        height: mediaWidth / 2.5,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () =>
+                          context.read<HomeScreenController>().pokemon3(),
+                      child: Container(
+                        width: mediaWidth / 5.5,
+                        height: mediaWidth / 2.5,
+                        color: Colors.yellow,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () =>
+                          context.read<HomeScreenController>().pokemon4(),
+                      child: Container(
+                        width: mediaWidth / 5.5,
+                        height: mediaWidth / 2.5,
+                        color: Colors.green,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () =>
+                          context.read<HomeScreenController>().pokemon5(),
+                      child: Container(
+                        width: mediaWidth / 5.5,
+                        height: mediaWidth / 2.5,
+                        color: Colors.pink,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- lib/home/home_screen_state.dart
+++ lib/home/home_screen_state.dart
@@ -15,7 +15,6 @@ abstract class HomeScreenState with _$HomeScreenState {
     @Default('') String pokemon3,
     @Default('') String pokemon4,
     @Default('') String pokemon5,
-
   }) = _HomeScreenState;
 }
 
@@ -23,9 +22,21 @@ class HomeScreenController extends StateNotifier<HomeScreenState>
     with LocatorMixin {
   HomeScreenController({
     required this.context,
+    this.isPokemon = true,
+    this.isPokemon1 = false,
+    this.isPokemon2 = false,
+    this.isPokemon3 = false,
+    this.isPokemon4 = false,
+    this.isPokemon5 = false,
   }) : super(const HomeScreenState());
 
   final BuildContext context;
+  bool isPokemon;
+  bool isPokemon1;
+  bool isPokemon2;
+  bool isPokemon3;
+  bool isPokemon4;
+  bool isPokemon5;
 
   @override
   void initState() {
@@ -35,6 +46,112 @@ class HomeScreenController extends StateNotifier<HomeScreenState>
   @override
   void dispose() {
     super.dispose();
+  }
+
+  void pokemon() {
+    isPokemon = true;
+    isPokemon1 = false;
+    isPokemon2 = false;
+    isPokemon3 = false;
+    isPokemon4 = false;
+    isPokemon5 = false;
+
+    print(isPokemon);
+  }
+
+  void pokemon1() {
+    isPokemon = false;
+    isPokemon1 = true;
+    isPokemon2 = false;
+    isPokemon3 = false;
+    isPokemon4 = false;
+    isPokemon5 = false;
+    print(isPokemon1);
+  }
+
+  void pokemon2() {
+    isPokemon = false;
+    isPokemon1 = false;
+    isPokemon2 = true;
+    isPokemon3 = false;
+    isPokemon4 = false;
+    isPokemon5 = false;
+    print(isPokemon2);
+  }
+
+  void pokemon3() {
+    isPokemon = false;
+    isPokemon1 = false;
+    isPokemon2 = false;
+    isPokemon3 = true;
+    isPokemon4 = false;
+    isPokemon5 = false;
+    print(isPokemon3);
+  }
+  void pokemon4() {
+    isPokemon = false;
+    isPokemon1 = false;
+    isPokemon2 = false;
+    isPokemon3 = false;
+    isPokemon4 = true;
+    isPokemon5 = false;
+    print(isPokemon4);
+  }
+
+  void pokemon5() {
+    isPokemon = false;
+    isPokemon1 = false;
+    isPokemon2 = false;
+    isPokemon3 = false;
+    isPokemon4 = false;
+    isPokemon5 = true;
+    print(isPokemon5);
+  }
+
+  void damage10() {
+
+
+
+
+    if(isPokemon) {
+      state = state.copyWith(
+        pokemon: '1',
+      );
+    }
+    if(isPokemon1) {
+      state = state.copyWith(
+        pokemon1: '2',
+      );
+    }
+    if(isPokemon2) {
+      state = state.copyWith(
+        pokemon2: '3',
+      );
+    }
+    if(isPokemon3) {
+      state = state.copyWith(
+        pokemon3: '4'
+      );
+    }
+    if(isPokemon4) {
+      state = state.copyWith(
+        pokemon4: '5',
+      );
+    }
+    if(isPokemon5) {
+      state = state.copyWith(
+        pokemon5: '6',
+      );
+    }
+
+  }
+
+  void damage50() {
+
+  }
+
+  void damage100() {
+
   }
 
 }
