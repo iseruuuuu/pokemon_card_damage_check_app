9742d2cc682801d0ea74237f7cac511bfa1a7d54
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-08-28 17:20:20 +0900

    fix: とりあえず、やけどと毒を削除

--- lib/component/other_card_widget.dart
+++ lib/component/other_card_widget.dart
@@ -6,11 +6,15 @@ class OtherCardWidget extends StatelessWidget {
     required this.mediaWidth,
     required this.text,
     required this.onTap,
+    required this.burn,
+    required this.poison,
   }) : super(key: key);
 
   final double mediaWidth;
   final String text;
   final Function() onTap;
+  final String burn;
+  final String poison;
 
   @override
   Widget build(BuildContext context) {
@@ -21,12 +25,33 @@ class OtherCardWidget extends StatelessWidget {
         height: mediaWidth / 2.5,
         color: Colors.green,
         child: Center(
-          child: Text(
-            text,
-            style: const TextStyle(
-              fontSize: 40,
-              color: Colors.black,
-            ),
+          child: Column(
+            children: [
+              const Spacer(),
+              Text(
+                burn,
+                style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                ),
+              ),
+              const Spacer(),
+              Text(
+                poison,
+                style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                ),
+              ),
+              const Spacer(),
+              Text(
+                text,
+                style: const TextStyle(
+                  fontSize: 25,
+                  color: Colors.black,
+                ),
+              ),
+            ],
           ),
         ),
       ),
--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -41,50 +41,131 @@ class HomeScreen extends StatelessWidget {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    ElevatedButton(
+                    TextButton(
                       onPressed: () => context.read<HomeScreenController>().reset(),
-                      child: const Text('リセット'),
+                      child: const Text(
+                          'リセット',
+                        style: TextStyle(
+                          fontSize: 20,
+
+                        ),
+                      ),
                     ),
                   ],
                 ),
+                const SizedBox(height: 30),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    GestureDetector(
+                      onTap: () =>
+                          context.read<HomeScreenController>().damage10(),
+                      child: Container(
+                        width: mediaWidth / 8,
+                        height: mediaWidth / 8,
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.red),
+                            borderRadius: BorderRadius.circular(30),
+                            color: Colors.red
+                        ),
+                        child: const Center(
+                          child: Text(
+                            '10',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () =>
+                          context.read<HomeScreenController>().damage50(),
+                      child: Container(
+                        width: mediaWidth / 8,
+                        height: mediaWidth / 8,
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.red),
+                            borderRadius: BorderRadius.circular(30),
+                            color: Colors.red
+                        ),
+                        child: const Center(
+                          child: Text(
+                            '50',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () =>
+                          context.read<HomeScreenController>().damage100(),
+                      child: Container(
+                        width: mediaWidth / 8,
+                        height: mediaWidth / 8,
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.red),
+                            borderRadius: BorderRadius.circular(30),
+                            color: Colors.red
+                        ),
+                        child: const Center(
+                          child: Text(
+                            '100',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+
                 const Spacer(),
                 Row(
                   children: [
                     const Spacer(),
-                    SizedBox(
-                      height: mediaWidth / 1.5,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Container(
-                            width: mediaWidth / 8,
-                            height: mediaWidth / 8,
-                            color: Colors.orange,
-                            child: const Center(
-                              child: Text(
-                                'やけど',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          ),
-                          Container(
-                            width: mediaWidth / 8,
-                            height: mediaWidth / 8,
-                            color: Colors.deepPurple,
-                            child: const Center(
-                              child: Text(
-                                'どく',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+                    // SizedBox(
+                    //   height: mediaWidth / 1.5,
+                    //   child: Column(
+                    //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    //     children: [
+                    //       Container(
+                    //         width: mediaWidth / 8,
+                    //         height: mediaWidth / 8,
+                    //         color: Colors.orange,
+                    //         child: const Center(
+                    //           child: Text(
+                    //             'やけど',
+                    //             style: TextStyle(
+                    //               color: Colors.white,
+                    //             ),
+                    //           ),
+                    //         ),
+                    //       ),
+                    //       Container(
+                    //         width: mediaWidth / 8,
+                    //         height: mediaWidth / 8,
+                    //         color: Colors.deepPurple,
+                    //         child: const Center(
+                    //           child: Text(
+                    //             'どく',
+                    //             style: TextStyle(
+                    //               color: Colors.white,
+                    //             ),
+                    //           ),
+                    //         ),
+                    //       ),
+                    //     ],
+                    //   ),
+                    // ),
                     const Spacer(),
                     GestureDetector(
                       onTap: () =>
@@ -93,14 +174,49 @@ class HomeScreen extends StatelessWidget {
                         width: mediaWidth / 2,
                         height: mediaWidth / 1.5,
                         color: Colors.red,
-                        child: Center(
-                          child: Text(
-                            '$pokemon',
-                            style: const TextStyle(
-                              fontSize: 40,
-                              color: Colors.white,
+                        child: Column(
+                          children: [
+                            const Spacer(),
+
+                            // Row(
+                            //   mainAxisAlignment: MainAxisAlignment.center,
+                            //   children: [
+                            //     const Spacer(),
+                            //     Center(
+                            //       child: Text(
+                            //           'やけど',
+                            //         style: TextStyle(
+                            //           fontSize: 20,
+                            //           color: Colors.white,
+                            //         ),
+                            //       ),
+                            //     ),
+                            //     const Spacer(),
+                            //     Center(
+                            //       child: Text(
+                            //         'どく',
+                            //         style: TextStyle(
+                            //           fontSize: 20,
+                            //           color: Colors.white,
+                            //         ),
+                            //       ),
+                            //     ),
+                            //     const Spacer(),
+                            //   ],
+                            // ),
+                            // const Spacer(),
+
+                            Center(
+                              child: Text(
+                                '$pokemon',
+                                style: const TextStyle(
+                                  fontSize: 40,
+                                  color: Colors.white,
+                                ),
+                              ),
                             ),
-                          ),
+                            const Spacer(),
+                          ],
                         ),
                       ),
                     ),
@@ -109,74 +225,7 @@ class HomeScreen extends StatelessWidget {
                       height: mediaWidth / 1.5,
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          GestureDetector(
-                            onTap: () =>
-                                context.read<HomeScreenController>().damage10(),
-                            child: Container(
-                              width: mediaWidth / 8,
-                              height: mediaWidth / 8,
-                              decoration: BoxDecoration(
-                                  border: Border.all(color: Colors.red),
-                                  borderRadius: BorderRadius.circular(30),
-                                  color: Colors.red
-                              ),
-                              child: const Center(
-                                child: Text(
-                                  '10',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          GestureDetector(
-                            onTap: () =>
-                                context.read<HomeScreenController>().damage50(),
-                            child: Container(
-                              width: mediaWidth / 8,
-                              height: mediaWidth / 8,
-                              decoration: BoxDecoration(
-                                  border: Border.all(color: Colors.red),
-                                  borderRadius: BorderRadius.circular(30),
-                                  color: Colors.red
-                              ),
-                              child: const Center(
-                                child: Text(
-                                  '50',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          GestureDetector(
-                            onTap: () =>
-                                context.read<HomeScreenController>().damage100(),
-                            child: Container(
-                              width: mediaWidth / 8,
-                              height: mediaWidth / 8,
-                              decoration: BoxDecoration(
-                                  border: Border.all(color: Colors.red),
-                                  borderRadius: BorderRadius.circular(30),
-                                  color: Colors.red
-                              ),
-                              child: const Center(
-                                child: Text(
-                                  '100',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
+                        children: [],
                       ),
                     ),
                     const Spacer(),
@@ -189,26 +238,36 @@ class HomeScreen extends StatelessWidget {
                     OtherCardWidget(
                       mediaWidth: mediaWidth,
                       text: '$pokemon1',
+                      burn: '',
+                      poison: '',
                       onTap: () => context.read<HomeScreenController>().pokemon1(),
                     ),
                     OtherCardWidget(
                       mediaWidth: mediaWidth,
                       text: '$pokemon2',
+                      burn: '',
+                      poison: '',
                       onTap: () => context.read<HomeScreenController>().pokemon2(),
                     ),
                     OtherCardWidget(
                       mediaWidth: mediaWidth,
                       text: '$pokemon3',
+                      burn: '',
+                      poison: '',
                       onTap: () => context.read<HomeScreenController>().pokemon3(),
                     ),
                     OtherCardWidget(
                       mediaWidth: mediaWidth,
                       text: '$pokemon4',
+                      burn: '',
+                      poison: '',
                       onTap: () => context.read<HomeScreenController>().pokemon4(),
                     ),
                     OtherCardWidget(
                       mediaWidth: mediaWidth,
                       text : '$pokemon5',
+                      burn: '',
+                      poison: '',
                       onTap: () => context.read<HomeScreenController>().pokemon5(),
                     ),
                   ],
